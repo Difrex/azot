875a517e39ede8a5b362b139024439ba59388ea5
Denis Zheleztsov <difrex@bingo-boom.ru>
2015-07-22 10:27:29 +0300

    resolution and cursor location

--- azot/X.py
+++ azot/X.py
@@ -1,2 +1,15 @@
-def get_resolution():
-    pass
+from Xlib import display
+
+# Get screen resolution
+def get_geometry():
+    width = display.Display().screen().width_in_pixels
+    height = display.Display().screen().height_in_pixels
+
+    return {"x": width, "y": height}
+
+
+# Get mouse cursor position
+def get_cursor_position():
+    data = display.Display().screen().root.query_pointer()._data
+
+    return {'x': data['root_x'], 'y': data['root_y']}
