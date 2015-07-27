9d028b26624a8f8f8e496c1c247c0954fefb9c26
Difrex <difrex.punk@gmail.com>
2015-07-27 16:52:11 +0300

    Fix crashes

--- azot/X.py
+++ azot/X.py
@@ -2,17 +2,22 @@ from Xlib import display
 
 # Get screen resolution
 def get_geometry():
-    try:
-        width = display.Display().screen().width_in_pixels
-        height = display.Display().screen().height_in_pixels
-        
-        return {"x": width, "y": height}
-    except:
-        get_geometry()
+    while 1:
+        try:
+            width = display.Display().screen().width_in_pixels
+            height = display.Display().screen().height_in_pixels
+            
+            return {"x": width, "y": height}
+        except:
+            pass
 
 
 # Get mouse cursor position
 def get_cursor_position():
-    data = display.Display().screen().root.query_pointer()._data
+    while 1:
+        try:
+            data = display.Display().screen().root.query_pointer()._data
 
-    return {'x': data['root_x'], 'y': data['root_y']}
+            return {'x': data['root_x'], 'y': data['root_y']}
+        except:
+            pass
