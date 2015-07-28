4724f54d87be6d3836bb212b4363f5884443e1e5
Difrex <difrex.punk@gmail.com>
2015-07-28 10:32:10 +0300

    stdout

--- azot/logger.py
+++ azot/logger.py
@@ -2,5 +2,11 @@ from __future__ import print_function
 import sys
 
 
+# Print to STDERR
 def warning(msg):
     print("WARNING: ", msg, file=sys.stderr)
+
+
+# Print to STDOUT
+def info(msg):
+    print("INFO: ", msg, file=sys.stdout)
